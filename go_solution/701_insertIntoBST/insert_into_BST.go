7494911be886f63159aaf051e8969228a6c20178
chinabobo <herbertyan@163.com>
2023-02-09 12:25:09 +0800

    update

---
+++ go_solution/701_insertIntoBST/insert_into_BST.go
@@ -0,0 +1,26 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// https://leetcode.cn/problems/insert-into-a-binary-search-tree/
+// 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。
+// 思路：找到最后一个叶子节点满足插入条件即可
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		root = &TreeNode{
+			Val: val,
+		}
+		return root
+	}
+	if root.Val < val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+	return root
+}
