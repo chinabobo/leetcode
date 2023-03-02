4a5811731903c45deb261b33fe5566c280797c37
chinabobo <herbertyan@163.com>
2023-03-02 11:55:09 +0800

    update

---
+++ go_solution/132_is_palindrome_linked_list/main.go
@@ -0,0 +1,141 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func newListNode(arr []int) *ListNode {
+	var head ListNode
+	var pre ListNode
+	for _, num := range arr {
+		node := ListNode{Val: num, Next: nil}
+		if head.Next == nil {
+			head.Next = &node
+		}
+		if pre.Next == nil {
+			pre.Next = &node
+		} else {
+			pre.Next.Next = &node
+			pre = *pre.Next
+		}
+	}
+	return head.Next
+}
+
+func main() {
+	list1 := newListNode([]int{1, 2, 2, 3, 3, 1})
+	fmt.Println(isPalindrome(list1))
+	fmt.Println(isPalindrome2(list1))
+}
+
+func isPalindrome(head *ListNode) bool {
+	var cou int
+	src := head
+	for head != nil {
+		cou++
+		head = head.Next
+	}
+	if cou&0x1 == 0 { // ou
+		return isPalindromeShuang(src, cou)
+	} else {
+		return isPalindromeOdd(src, cou)
+	}
+}
+
+func isPalindromeShuang(head *ListNode, cou int) bool {
+	tag := false
+	mid := cou / 2
+	var stack []int
+	if head == nil {
+		return false
+	}
+	if head.Next == nil {
+		return true
+	}
+	for head != nil {
+		if len(stack) == mid {
+			tag = true
+		}
+		if len(stack) != 0 && stack[len(stack)-1] == head.Val && tag {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, head.Val)
+		}
+		fmt.Println(stack)
+		head = head.Next
+	}
+	if len(stack) == 0 {
+		return true
+	}
+	return false
+}
+
+func isPalindromeOdd(head *ListNode, cou int) bool {
+	if head.Next == nil {
+		return true
+	}
+	var n int
+	m := cou/2 - 1
+	fmt.Println(m)
+
+	src := head
+	for head != nil {
+		if n == m {
+			head.Next = head.Next.Next
+			fmt.Println(n)
+			fmt.Println(head.Val)
+			break
+		}
+		n++
+		head = head.Next
+	}
+	return isPalindromeShuang(src, cou-1)
+}
+
+func isPalindrome2(head *ListNode) bool {
+	// 1 2 nil
+	// 1 2 1 nil
+	// 1 2 2 1 nil
+	if head == nil {
+		return true
+	}
+	slow := head
+	// fast如果初始化为head.Next则中点在slow.Next
+	// fast初始化为head,则中点在slow
+	fast := head.Next
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	tail := reverse(slow.Next)
+	// 断开两个链表(需要用到中点前一个节点)
+	slow.Next = nil
+	for head != nil && tail != nil {
+		if head.Val != tail.Val {
+			return false
+		}
+		head = head.Next
+		tail = tail.Next
+	}
+	return true
+
+}
+
+func reverse(head *ListNode) *ListNode {
+	// 1->2->3
+	if head == nil {
+		return head
+	}
+	var prev *ListNode
+	for head != nil {
+		t := head.Next
+		head.Next = prev
+		prev = head
+		head = t
+	}
+	return prev
+}
--- go_solution/142_linked_list_cycle_ii/README.md
+++ go_solution/142_linked_list_cycle_ii/README.md
@@ -7,5 +7,35 @@ a + n ( b + c ) + b - 1 = 2(a + b)
 a = (n - 1) (b + c) + c - 1
 
 快慢指针，快慢相遇之后，慢指针回到头，快指针向后一个节点（由于一开始快指针是从`fast := head.Next`开始的），快慢指针步调一致一起移动，相遇点即为入环点。
+第一次相交后，快指针需要从下一个节点开始和头指针一起匀速移动
 
+另外一种方式是 fast=head,slow=head
+```go
+func detectCycle(head *ListNode) *ListNode {
+    // 思路：快慢指针，快慢相遇之后，其中一个指针回到头，快慢指针步调一致一起移动，相遇点即为入环点
+    // nb+a=2nb+a
+    if head == nil {
+        return head
+    }
+    fast := head
+    slow := head
 
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        slow = slow.Next
+        if fast == slow {
+            // 指针重新从头开始移动
+            fast = head
+            for fast != slow {
+                fast = fast.Next
+                slow = slow.Next
+            }
+            return slow
+        }
+    }
+    return nil
+}
+```
+这两种方式不同点在于，一般用 fast=head.Next 较多，因为这样可以知道中点的上一个节点，可以用来删除等操作。
+- fast 如果初始化为 head.Next 则中点在 slow.Next
+- fast 初始化为 head,则中点在 slow
