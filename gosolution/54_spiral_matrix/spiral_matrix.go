14eaaf959d79caac5c62ed41b6304fd2403eb4c8
herbert <herbertyanbb@gmail.com>
2023-05-19 00:12:06 +0800

    update 54, 59

---
+++ gosolution/54_spiral_matrix/spiral_matrix.go
@@ -0,0 +1,96 @@
+package main
+
+/*
+https://leetcode.cn/problems/spiral-matrix/solution/luo-xuan-ju-zhen-by-leetcode-solution/
+给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+
+可以模拟螺旋矩阵的路径。初始位置是矩阵的左上角，初始方向是向右，当路径超出界限或者进入之前访问过的位置时，顺时针旋转，进入下一个方向。
+
+判断路径是否进入之前访问过的位置需要使用一个与输入矩阵大小相同的辅助矩阵visited
+其中的每个元素表示该位置是否被访问过。当一个元素被访问时，将 visited 中的对应位置的元素设为已访问。
+
+如何判断路径是否结束？
+由于矩阵中的每个元素都被访问一次，因此路径的长度即为矩阵中的元素数量，当路径的长度达到矩阵中的元素数量时即为完整路径，将该路径返回。
+
+*/
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return []int{}
+	}
+	rows, columns := len(matrix), len(matrix[0])
+	visited := make([][]bool, rows)
+	for i := 0; i < rows; i++ {
+		visited[i] = make([]bool, columns)
+	}
+
+	var (
+		total          = rows * columns
+		order          = make([]int, total)
+		row, column    = 0, 0
+		directions     = [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+		directionIndex = 0
+	)
+
+	for i := 0; i < total; i++ {
+		order[i] = matrix[row][column]
+		visited[row][column] = true
+		nextRow, nextColumn := row+directions[directionIndex][0], column+directions[directionIndex][1]
+		if nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn] {
+			directionIndex = (directionIndex + 1) % 4
+		}
+		row += directions[directionIndex][0]
+		column += directions[directionIndex][1]
+	}
+	return order
+}
+
+/*
+可以将矩阵看成若干层，首先输出最外层的元素，其次输出次外层的元素，直到输出最内层的元素。
+
+定义矩阵的第 k 层是到最近边界距离为 k 的所有顶点。例如，下图矩阵最外层元素都是第 1 层，次外层元素都是第 2 层，剩下的元素都是第 3 层。
+
+[[1, 1, 1, 1, 1, 1, 1],
+ [1, 2, 2, 2, 2, 2, 1],
+ [1, 2, 3, 3, 3, 2, 1],
+ [1, 2, 2, 2, 2, 2, 1],
+ [1, 1, 1, 1, 1, 1, 1]]
+
+*/
+
+func spiralOrder2(matrix [][]int) []int {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return []int{}
+	}
+	var (
+		rows, columns            = len(matrix), len(matrix[0])
+		order                    = make([]int, rows*columns)
+		index                    = 0
+		left, right, top, bottom = 0, columns - 1, 0, rows - 1
+	)
+
+	for left <= right && top <= bottom {
+		for column := left; column <= right; column++ {
+			order[index] = matrix[top][column]
+			index++
+		}
+		for row := top + 1; row <= bottom; row++ {
+			order[index] = matrix[row][right]
+			index++
+		}
+		if left < right && top < bottom {
+			for column := right - 1; column > left; column-- {
+				order[index] = matrix[bottom][column]
+				index++
+			}
+			for row := bottom; row > top; row-- {
+				order[index] = matrix[row][left]
+				index++
+			}
+		}
+		left++
+		right--
+		top++
+		bottom--
+	}
+	return order
+}
---
+++ gosolution/59_spiral_matrix_ii/spairal_matrix_ii.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+https://leetcode.cn/problems/spiral-matrix-ii/
+
+Time complexity : O(n^2)
+Space complexity : O(1)
+*/
+func generateMatrix(n int) [][]int {
+	matrix := make([][]int, n)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
+	}
+	num := 1
+	left, right, top, bottom := 0, n-1, 0, n-1
+	for left <= right && top <= bottom {
+		for column := left; column <= right; column++ {
+			matrix[top][column] = num
+			num++
+		}
+		for row := top + 1; row <= bottom; row++ {
+			matrix[row][right] = num
+			num++
+		}
+		if left < right && top < bottom {
+			for column := right - 1; column > left; column-- {
+				matrix[bottom][column] = num
+				num++
+			}
+			for row := bottom; row > top; row-- {
+				matrix[row][left] = num
+				num++
+			}
+		}
+		left++
+		right--
+		top++
+		bottom--
+	}
+	return matrix
+}
