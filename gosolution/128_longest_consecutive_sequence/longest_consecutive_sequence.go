cd947cdbaa4b5556699de6737058fd76c6ad45a3
herbert <herbertyanbb@gmail.com>
2023-05-27 00:40:46 +0800

    update 128

---
+++ gosolution/128_longest_consecutive_sequence/longest_consecutive_sequence.go
@@ -0,0 +1,34 @@
+package main
+
+/*
+https://leetcode.cn/problems/longest-consecutive-sequence
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+
+思路：找到最长序列的第一个数字
+外层循环需要 O(n) 的时间复杂度，只有当一个数是连续序列的第一个数的情况下才会进入内层循环，然后在内层循环中匹配连续序列中的数，因此数组中的每个数只会进入内层循环一次。
+根据上述分析可知，总时间复杂度为 O(n)，符合题目要求。
+
+*/
+func longestConsecutive(nums []int) int {
+	numSet := map[int]bool{}
+	for _, num := range nums {
+		numSet[num] = true
+	}
+	longestStreak := 0
+	for num := range numSet {
+		if !numSet[num-1] {
+			currentNum := num
+			currentStreak := 1
+			for numSet[currentNum+1] {
+				currentNum++
+				currentStreak++
+			}
+			if longestStreak < currentStreak {
+				longestStreak = currentStreak
+			}
+		}
+	}
+	return longestStreak
+}
