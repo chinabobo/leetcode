4a701a750a432f5b351cd4140714de6d77f23332
herbert <herbertyanbb@gmail.com>
2023-10-27 10:56:40 +0800

    update 76

---
+++ gosolution/76_minimum_window_substring/minimum_window_substring.go
@@ -0,0 +1,76 @@
+package main
+
+import "math"
+
+/*
+给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+注意：
+
+对于 t 中重复字符，我们寻找的子字符串中该字符数量必须不少于 t 中该字符数量。
+如果 s 中存在这样的子串，我们保证它是唯一的答案。
+
+input s = "ADOBECODEBANC", t = "ABC"
+output "BANC"
+
+输入: s = "a", t = "aa"
+输出: ""
+解释: t 中两个字符 'a' 均应包含在 s 的子串中，因此没有符合条件的子字符串，返回空字符串。
+
+*/
+
+func minWindow(s string, t string) string {
+	// 保存滑动窗口字符集
+	win := make(map[byte]int)
+	// 保存需要的字符集
+	need := make(map[byte]int)
+	for i := 0; i < len(t); i++ {
+		need[t[i]]++
+	}
+	// 窗口
+	left := 0
+	right := 0
+	// match匹配次数
+	match := 0
+	start := 0
+	end := 0
+	min := math.MaxInt64
+	var c byte
+	for right < len(s) {
+		c = s[right]
+		right++
+		// 在需要的字符集里面，添加到窗口字符集里面
+		if need[c] != 0 {
+			win[c]++
+			// 如果当前字符的数量匹配需要的字符的数量，则match值+1
+			if win[c] == need[c] {
+				match++
+			}
+		}
+
+		// 当所有字符数量都匹配之后，开始缩紧窗口
+		for match == len(need) {
+			// 获取结果
+			if right-left < min {
+				min = right - left
+				start = left
+				end = right
+			}
+			c = s[left]
+			left++
+			// 左指针指向不在需要字符集则直接跳过
+			if need[c] != 0 {
+				// 左指针指向字符数量和需要的字符相等时，右移之后match值就不匹配则减一
+				// 因为win里面的字符数可能比较多，如有10个A，但需要的字符数量可能为3
+				// 所以在压死骆驼的最后一根稻草时，match才减一，这时候才跳出循环
+				if win[c] == need[c] {
+					match--
+				}
+				win[c]--
+			}
+		}
+	}
+	if min == math.MaxInt64 {
+		return ""
+	}
+	return s[start:end]
+}
